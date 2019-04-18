4f383fd7cbfa93856f1e5d747b709b8e2258a8b0
ZedYeung <shoominga@gmail.com>
2019-04-18 11:58:03 -0700

    add utils

--- InstaHappy/InstaHappy/Controller/LoginViewController.swift
+++ InstaHappy/InstaHappy/Controller/LoginViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var lblStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblStatus.isHidden = true
     }
 
 
+    @IBAction func LoginPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ForgotPasswordPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func SignupPressed(_ sender: Any) {
+    }
 }
 
---
+++ InstaHappy/InstaHappy/Utils/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  InstaHappy
+//
+//  Created by Zed on 4/18/19.
+//  Copyright © 2019 Zed. All rights reserved.
+//
+
+import Foundation
