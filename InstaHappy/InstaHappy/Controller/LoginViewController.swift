e8b0bd684d8a2d2db25cdbe52732ffac7141a652
Yue (Zed) Yang <shoominga@gmail.com>
2019-04-18 12:06:54 -0700

    add login segue

--- InstaHappy/InstaHappy/Controller/LoginViewController.swift
+++ InstaHappy/InstaHappy/Controller/LoginViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import SVProgressHUD
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
-    
+
     @IBOutlet weak var lblStatus: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,13 +25,39 @@ class LoginViewController: UIViewController {
 
 
     @IBAction func LoginPressed(_ sender: Any) {
+        let email = txtEmail.text!
+        let password = txtPassword.text!
+
+        if email.isEmail == false{
+            lblStatus.text = "Enter a valid e mail"
+            return
+        }
+        if password.count < 6 {
+            lblStatus.text = "Enter correct password"
+            return
+        }
+
+        SVProgressHUD.show()
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] user, error in
+            guard let strongSelf = self else { return }
+
+            SVProgressHUD.dismiss()
+            if error == nil{
+               // perform segue
+                strongSelf.performSegue(withIdentifier: "LoginSegue", sender: strongSelf)
+            }
+            else{
+                strongSelf.lblStatus.text = error?.localizedDescription
+                return
+            }
+        }
     }
-    
-    
+
+
     @IBAction func ForgotPasswordPressed(_ sender: Any) {
     }
-    
-    
+
+
     @IBAction func SignupPressed(_ sender: Any) {
     }
 }
