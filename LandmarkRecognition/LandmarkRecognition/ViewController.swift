17c6294b24e5fae34a42e5f56db5b56bcd00535f
Yue (Zed) Yang <shoominga@gmail.com>
2019-04-19 10:47:04 -0700

    add camera for imagePicker

--- LandmarkRecognition/LandmarkRecognition/ViewController.swift
+++ LandmarkRecognition/LandmarkRecognition/ViewController.swift
@@ -10,45 +10,61 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var result: UITextView!
-    
-    let imagePicker = UIImagePickerController()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        imagePicker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
     }
 
     @IBAction func uploadImage(_ sender: UIButton) {
-        imagePicker.allowsEditing = false
-        imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
+        let imagePicker = UIImagePickerController()
+
+        imagePicker.delegate = self
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            } else {
+                print("Camera Not Available!")
+            }
+        }))
+
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (UIAlertAction) in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+
+        self.present(actionSheet, animated: true, completion: nil)
     }
-    
+
     @IBAction func recognize(_ sender: UIButton) {
         detectCloudLandmarks(image: image.image)
     }
-    
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         self.image.image = pickedImage
         picker.dismiss(animated: true, completion: nil)
     }
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
-    
+
     func detectCloudLandmarks(image: UIImage?) {
         let vision = Vision.vision()
         guard let image = image else { return }
-        
+
         // Initialize a VisionImage object with the given UIImage.
         let visionImage = VisionImage(image: image)
-        
+
         // Create a landmark detector.
         // [START config_landmark_cloud]
         let options = VisionCloudDetectorOptions()
@@ -69,11 +85,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 // [END_EXCLUDE]
                 return
             }
-            
+
             // Recognized landmarks
             // [START_EXCLUDE]
             self.result.text = landmarks.map { landmark -> String in
-                
+
                 return "Landmark: \(String(describing: landmark.landmark ?? "")), " +
                     "Confidence: \(String(describing: landmark.confidence ?? 0) ), " +
                     "EntityID: \(String(describing: landmark.entityId ?? "") ), " +
